9b93c250e5ac75d077b172c0411c474971d4ce94
Jonas Zaddach <zaddach@eurecom.fr>
2013-04-05 13:52:11 +0200

    Qemu: provide a memory-mapped serial port for ARM
    This commit slightly modifies the UART 16550 serial port so that
    it can be freely instantiated on ARM machines as a mmapped device
    (instead of only X86 io-port device).
    
    Signed-off-by: Jonas Zaddach <zaddach@eurecom.fr>

--- hw/serial.c
+++ hw/serial.c
@@ -28,6 +28,7 @@
 #include "pc.h"
 #include "qemu-timer.h"
 #include "sysemu.h"
+#include "sysbus.h"
 
 //#define DEBUG_SERIAL
 
@@ -164,6 +165,14 @@ typedef struct ISASerialState {
     uint32_t isairq;
     SerialState state;
 } ISASerialState;
+
+typedef struct SysbusSerialState {
+    SysBusDevice dev;
+    uint32_t index;
+    uint32_t iobase;
+    uint32_t irq;
+    SerialState state;
+} SysbusSerialState;
 
 static void serial_receive1(void *opaque, const uint8_t *buf, int size);
 
@@ -809,6 +818,16 @@ static const VMStateDescription vmstate_isa_serial = {
     }
 };
 
+static const VMStateDescription vmstate_sysbus_serial = {
+    .name = "serial",
+    .version_id = 3,
+    .minimum_version_id = 2,
+    .fields      = (VMStateField []) {
+        VMSTATE_STRUCT(state, SysbusSerialState, 0, vmstate_serial, SerialState),
+        VMSTATE_END_OF_LIST()
+    }
+};
+
 SerialState *serial_init(int base, qemu_irq irq, int baudbase,
                          CharDriverState *chr)
 {
@@ -887,12 +906,54 @@ SerialState *serial_mm_init(MemoryRegion *address_space,
     return s;
 }
 
+
+static int serial_sysbus_initfn(SysBusDevice *dev)
+{
+    static int index;
+    SysbusSerialState *sysbus = DO_UPCAST(SysbusSerialState, dev, dev);
+    SerialState *s = &sysbus->state;
+
+    if (sysbus->index == -1)
+        sysbus->index = index;
+    if (sysbus->index >= MAX_SERIAL_PORTS)
+        return -1;
+//    if (sysbus->iobase == -1)
+//        return -1;
+//        sysbus->iobase = isa_serial_io[sysbus->index];
+//    if (sysbus->isairq == -1)
+//        sysbus->isairq = isa_serial_irq[isa->index];
+    index++;
+
+    s->baudbase = 115200;
+    s->chr = qemu_char_get_next_serial();
+    s->it_shift = 2;
+
+    sysbus_init_irq(dev, &s->irq);
+//    isa_init_irq(dev, &s->irq, isa->isairq);
+    serial_init_core(s);
+//    qdev_set_legacy_instance_id(&dev->qdev, isa->iobase, 3);
+
+    memory_region_init_io(&s->io, &serial_mm_ops[0], s, "serial", 8 << s->it_shift);
+    sysbus_init_mmio(dev, &s->io);
+//    isa_register_ioport(dev, &s->io, isa->iobase);
+    return 0;
+}
+
 static Property serial_isa_properties[] = {
     DEFINE_PROP_UINT32("index", ISASerialState, index,   -1),
     DEFINE_PROP_HEX32("iobase", ISASerialState, iobase,  -1),
     DEFINE_PROP_UINT32("irq",   ISASerialState, isairq,  -1),
     DEFINE_PROP_CHR("chardev",  ISASerialState, state.chr),
     DEFINE_PROP_UINT32("wakeup", ISASerialState, state.wakeup, 0),
+    DEFINE_PROP_END_OF_LIST(),
+};
+
+static Property serial_sysbus_properties[] = {
+    DEFINE_PROP_UINT32("index", SysbusSerialState, index,   -1),
+    DEFINE_PROP_HEX32("iobase", SysbusSerialState, iobase,  -1),
+    DEFINE_PROP_UINT32("irq",   SysbusSerialState, irq,  -1),
+    DEFINE_PROP_CHR("chardev",  SysbusSerialState, state.chr),
+    DEFINE_PROP_UINT32("wakeup", SysbusSerialState, state.wakeup, 0),
     DEFINE_PROP_END_OF_LIST(),
 };
 
@@ -905,6 +966,15 @@ static void serial_isa_class_initfn(ObjectClass *klass, void *data)
     dc->props = serial_isa_properties;
 }
 
+static void serial_sysbus_class_initfn(ObjectClass *klass, void *data)
+{
+    DeviceClass *dc = DEVICE_CLASS(klass);
+    SysBusDeviceClass *ic = SYS_BUS_DEVICE_CLASS(klass);
+    ic->init = serial_sysbus_initfn;
+    dc->vmsd = &vmstate_sysbus_serial;
+    dc->props = serial_sysbus_properties;
+}
+
 static TypeInfo serial_isa_info = {
     .name          = "isa-serial",
     .parent        = TYPE_ISA_DEVICE,
@@ -912,9 +982,17 @@ static TypeInfo serial_isa_info = {
     .class_init    = serial_isa_class_initfn,
 };
 
+static TypeInfo serial_sysbus_info = {
+    .name          = "sysbus-serial",
+    .parent        = TYPE_SYS_BUS_DEVICE,
+    .instance_size = sizeof(SysbusSerialState),
+    .class_init    = serial_sysbus_class_initfn,
+};
+
 static void serial_register_types(void)
 {
     type_register_static(&serial_isa_info);
+    type_register_static(&serial_sysbus_info);
 }
 
 type_init(serial_register_types)
