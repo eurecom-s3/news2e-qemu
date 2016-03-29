c2349bdd15d17a428281c26558bfd861fc4664ee
Jonas Zaddach <zaddach@eurecom.fr>
2016-03-29 12:23:34 +0000

    Added test for OnTranslateInstructionEnd event

---
+++ s2e/Plugins/test/TestOnTranslateInstructionEnd.cpp
@@ -0,0 +1,44 @@
+#include "s2e/cxx/Plugin.h" 
+#include "s2e/Plugins/CorePlugin.h"
+#include "s2e/cxx/S2EExecutionState.h"
+#include "s2e/cxx/S2EExecutor.h"
+#include "s2e/cxx/ConfigFile.h"
+#include "s2e/cxx/Utils.h"
+#include "s2e/cxx/S2E.h"
+
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+namespace s2e {
+namespace plugins {
+
+class TestOnTranslateInstructionEndPlugin : public Plugin
+{
+	S2E_PLUGIN
+
+public:
+	TestOnTranslateInstructionEndPlugin(S2E* s2e): Plugin(s2e) {}
+	void initialize();
+private:
+	void slotTranslateInsnEnd(ExecutionSignal* signal, S2EExecutionState* state, TranslationBlock* tb, uint64_t pc);
+};
+
+S2E_DEFINE_PLUGIN(TestOnTranslateInstructionEndPlugin, "Print when the OnTranslateInstructionStart event is received", "TestOnTranslateInstructionEnd");
+
+void TestOnTranslateInstructionEndPlugin::initialize()
+{
+    s2e()->getCorePlugin()->onTranslateInstructionEnd.connect(sigc::mem_fun(*this, &TestOnTranslateInstructionEndPlugin::slotTranslateInsnEnd));
+}
+
+void TestOnTranslateInstructionEndPlugin::slotTranslateInsnEnd(ExecutionSignal *signal,
+                                    S2EExecutionState *state,
+                                    TranslationBlock *tb,
+                                    uint64_t pc)
+{
+    s2e()->getWarningsStream() << "OnTranslateInstructionEnd called at pc 0x" << hexval(pc) << '\n';
+}
+
+} // namespace plugins
+} // namespace s2e
