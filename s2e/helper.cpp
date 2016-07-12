f46dad9a391d1466b9605de89c9c7fd809956f2f
Jonas Zaddach <zaddach@eurecom.fr>
2016-07-12 12:10:38 +0000

    Removed s2e_ld/st helper functions

--- s2e/helper.cpp
+++ s2e/helper.cpp
@@ -52,67 +52,6 @@ static uint64_t Env_getPc(CPUArchState* env)
 #endif /* defined(TARGET_ARM) */
 }
 
-
-uint64_t helper_s2e_ld(CPUArchState* env, uint64_t addr, uint32_t memop, uint32_t idx)
-{
-	printf("mem_read(addr = 0x%08" PRIx32 ", size = %d, idx = %d)\n", (uint32_t) addr, (int) (memop & MO_SIZE), (int) idx);
-	uint64_t v = 0;
-	switch (memop & MO_BSWAP) {
-		case MO_BE:
-			switch (memop & MO_SIZE) {
-				case MO_8:  v = helper_ret_ldub_mmu(env, addr, idx, 0); break;
-				case MO_16: v = helper_be_lduw_mmu(env, addr, idx, 0); break;
-				case MO_32: v = helper_be_ldul_mmu(env, addr, idx, 0); break;
-				case MO_64: v = helper_be_ldq_mmu(env, addr, idx, 0); break;
-				default: abort();
-			}
-			break;
-		case MO_LE:
-			switch (memop & MO_SIZE) {
-				case MO_8:  v = helper_ret_ldub_mmu(env, addr, idx, 0); break;   
-				case MO_16: v = helper_le_lduw_mmu(env, addr, idx, 0); break;
-				case MO_32: v = helper_le_ldul_mmu(env, addr, idx, 0); break;
-				case MO_64: v = helper_le_ldq_mmu(env, addr, idx, 0); break;
-				default: abort();
-			}
-			break;
-		default:
-			abort();
-	}
-
-	if (memop & MO_SIGN) {
-		v = sign_extend(v, memop & MO_SIZE);
-	}
-	return v;
-}
-
-void helper_s2e_st(CPUArchState* env, uint64_t addr, uint32_t memop, uint32_t idx, uint64_t val)
-{
-	printf("mem_write(addr = 0x%08" PRIx32 ", size = %d, idx = %d, val = 0x%08" PRIx32 ")\n", (uint32_t) addr, (int) (memop & MO_SIZE), (int) idx, (uint32_t) val);
-	switch (memop & MO_BSWAP) {
-		case MO_BE:
-			switch (memop & MO_SIZE) {
-				case MO_8:  helper_ret_stb_mmu(env, addr, val, idx, 0); break;
-				case MO_16: helper_be_stw_mmu(env, addr, val, idx, 0); break;
-				case MO_32: helper_be_stl_mmu(env, addr, val, idx, 0); break;
-				case MO_64: helper_be_stq_mmu(env, addr, val, idx, 0); break;
-				default: abort();
-			}
-			break;
-		case MO_LE:
-			switch (memop & MO_SIZE) {
-				case MO_8:  helper_ret_stb_mmu(env, addr, val, idx, 0); break;   
-				case MO_16: helper_le_stw_mmu(env, addr, val, idx, 0); break;
-				case MO_32: helper_le_stl_mmu(env, addr, val, idx, 0); break;
-				case MO_64: helper_le_stq_mmu(env, addr, val, idx, 0); break;
-				default: abort();
-			}
-			break;
-		default:
-			abort();
-	}
-}
-
 void helper_s2e_base_instruction(CPUArchState* env, uint32_t op_idx)
 {
 	CPUState* cpu = CPU(ENV_GET_CPU(env));
